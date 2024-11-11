982085eeb4823f5a61ff1f4228f0de2577f22073
Efraim Costa <efra10_efraim@hotmail.com>
2024-11-11 15:00:02 -0300

    Configuração do renderizador HTML mustache-express.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express")
 const app = express()
+const mustacheExpress = require("mustache-express")
 
+//Renderizador
+app.engine("html", mustacheExpress)
+app.set("view engine", "html")
+app.set("view", __dirname + "/src/view")
+
+//Servidor
 const PORT = 8080
 app.listen(PORT, function(){
     console.log(`Aplicação rodando na porta ${PORT}`)
